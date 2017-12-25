683cae01765928886eb3b86f8061aade60ad03b0
asankagit <inbox2asanka@gmail.com>
2017-12-25 12:26:40 +0530

    active android m2m

--- app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -14,9 +14,9 @@ public final class R {
         public static final int stackFromEnd = 0x7f0100c1;
     }
     public static final class dimen {
-        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f060058;
-        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f060059;
-        public static final int item_touch_helper_swipe_escape_velocity = 0x7f06005a;
+        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f06005a;
+        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f06005b;
+        public static final int item_touch_helper_swipe_escape_velocity = 0x7f06005c;
     }
     public static final class id {
         public static final int item_touch_helper_previous_elevation = 0x7f0a0005;
--- app/src/main/java/com/example/android/effectivenavigation/model/UserItem.java
+++ app/src/main/java/com/example/android/effectivenavigation/model/UserItem.java
@@ -1,18 +1,27 @@
 package com.example.android.effectivenavigation.model;
 
+import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
+
+import java.util.List;
 
 /**
  * Created by asanka on 11/22/17.
  */
 
 @Table(name = "UserItem")
-public class UserItem {
+public class UserItem  extends Model {
 
-    @Column(name = "User")
+    @Column(name = "foo", onDelete= Column.ForeignKeyAction.CASCADE)
+    public Item item;
+    @Column(name = "bar", onDelete= Column.ForeignKeyAction.CASCADE)
     public User user;
 
-    @Column(name = "Item")
-    public Item item;
+    public List<Item> item_list() {
+        return getMany(Item.class, "UserItem");
+    }
+    public List<User> user_list() {
+        return getMany(User.class, "UserItem");
+    }
 }
