91fe7ae868d72549dd4482be27a961597b9a5355
asankagit <inbox2asanka@gmail.com>
2018-03-31 16:46:41 +0530

    add pageViewer

--- app/src/main/java/com/google/firebase/quickstart/effectivenavigation/reports/DailyIncome.java
+++ app/src/main/java/com/google/firebase/quickstart/effectivenavigation/reports/DailyIncome.java
@@ -1,8 +1,11 @@
 package com.google.firebase.quickstart.effectivenavigation.reports;
 
+import android.os.Build;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.DatePicker;
 
 import com.example.android.effectivenavigation.R;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
@@ -15,20 +18,48 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.quickstart.effectivenavigation.dbConn.DBConn;
 import com.google.firebase.quickstart.effectivenavigation.models.Income;
 import com.google.firebase.quickstart.effectivenavigation.models.Post;
+import com.google.firebase.quickstart.effectivenavigation.viewholder.PageViewAdapter;
 import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class DailyIncome extends AppCompatActivity {
     public ArrayList<Income>  incomeArrList = new ArrayList<Income>();
+    DatePicker datePicker;
+
+    PageViewAdapter mAdapter;
+    ViewPager mPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_income);
+        datePicker = (DatePicker)  findViewById(R.id.datePicker);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        datePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                Log.d("Date", "Year=" + year + " Month=" + (month + 1) + " day=" + dayOfMonth);
+
+            }
+        });
+
+        /**pageview **/
+        mAdapter = new PageViewAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager)findViewById(R.id.viewPager);
+        mPager.setAdapter(mAdapter);
+        mPager.setCurrentItem(0);
+        /**page view**/
+
 //        drawGraph();
         dailyIncome();
 
@@ -40,23 +71,16 @@ public class DailyIncome extends AppCompatActivity {
         GraphView graph = (GraphView) findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(
                 dataPoint
-//                new DataPoint[] {
-//                        new DataPoint(0, 10000),
-//                        new DataPoint(3, 20000),
-//                        new DataPoint(14, 60000),
-//                        new DataPoint(1, 50000),
-//                        new DataPoint(22, 30000)
-//                }
         );
 
         series.setTitle("Random Curve 1");
 //        series.setColor(254);
         series.setDrawDataPoints(true);
-        series.setDataPointsRadius(10);
-        series.setThickness(8);
+        series.setDataPointsRadius(5);
+        series.setThickness(3);
         graph.addSeries(series);
 
-        /**
+
         graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
             @Override
             public String formatLabel(double value, boolean isValueX) {
@@ -68,12 +92,14 @@ public class DailyIncome extends AppCompatActivity {
                     return super.formatLabel(value, isValueX) + " Rs";
                 }
             }
-        });**/
+        });
 /**************** end graph*/
     }
 
+
+
     static int count = 0 ;
-    static Income  dailyIncome;
+    Income  dailyIncome;
 
     ArrayList<DataPoint> dp = new ArrayList<>();
 
@@ -92,36 +118,9 @@ public class DailyIncome extends AppCompatActivity {
 
                 System.out.println("The OnChildAdd " + snapshot.getKey() + " dinosaur's score is " + snapshot.getValue());
                 dailyIncome = snapshot.getValue(Income.class);
-
-
-//                System.out.println("DAILY_INCOME__"+snapshot.child("name").getValue());
-//              System.out.println("DAILY_INCOME_NAME_"+dailyIncome.name);
-
                 incomeArrList.add(dailyIncome);
                 dp.add(new DataPoint(dailyIncome.getDay(),dailyIncome.amount));
 
-//              printData();
-                System.out.println("count"+count);
-                count++;
-
-
-
-//                new DataPoint[] {
-//                        new DataPoint(0, 1),
-//                        new DataPoint(1, 5),
-//                        new DataPoint(2, 3),
-//                        new DataPoint(3, 2),
-//                        new DataPoint(4, 6)
-//                }
-//                dp[0] = new DataPoint(0,0);
-
-//               for(int i =0; i<incomeArrList.size();i++){
-//
-//                   dp.add(new DataPoint(incomeArrList.get(i).getDay(),incomeArrList.get(i).amount));
-//                   dp.add(new DataPoint(3,0));
-//                   System.out.println(incomeArrList.size()+"DATE__" +incomeArrList.get(i).getDay()+"*"+incomeArrList.get(i).amount);
-//
-//                }
 
                 DataPoint[] dataPoint = new DataPoint[dp.size()];
                 dataPoint = dp.toArray(dataPoint);
@@ -150,45 +149,9 @@ public class DailyIncome extends AppCompatActivity {
 
             }
         });
-        /*****/
-        /**********************************************/
-        Query postsQuery = DBConn.getDatabase().getReference().child("income").orderByChild("name").equalTo("asa");
-        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<Post>()
-                .setQuery(postsQuery, Post.class)
-                .build();
-        System.out.println("OPTIONS>>>"+options);
-        /*********************************************/
 
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                Post post = snapshot.getValue(Post.class);
-                System.out.println("<><><><"+post.body);
-                System.out.println("<><><><"+post.title);
-                System.out.println("<><><><"+post.author);
-                if (post.author!=null) {
-                    System.out.println("connected");
-                } else {
-                    System.out.println("not connected");
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                System.err.println("Listener was cancelled");
-            }
-        });
-        System.out.println(">>>>>>>>>>>>>>>>>>>>"+myRef.child("post"));
 
 
     }
 
-    public void printData(){
-        while(!incomeArrList.isEmpty()){
-            String tmp_date =  incomeArrList.get(0).date;
-            Double tmp_amount =  incomeArrList.get(0).amount;
-            System.out.println("DATE_"+tmp_date+"AMOUNT_"+tmp_amount);
-        }
-
-    }
 }
---
+++ app/src/main/java/com/google/firebase/quickstart/effectivenavigation/viewholder/PageViewAdapter.java
@@ -0,0 +1,123 @@
+package com.google.firebase.quickstart.effectivenavigation.viewholder;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.android.effectivenavigation.R;
+import com.google.firebase.quickstart.effectivenavigation.fragment.InvoiceApproved;
+
+/**
+ * Created by DELL on 3/31/2018.
+ */
+
+public  class PageViewAdapter  extends FragmentPagerAdapter {
+    /*static*/ final int NUM_ITEMS = 2;
+
+    public PageViewAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if(position==1)
+            return ArrayListFragment.newInstance(position);
+        else
+            return ArticleFragment.newInstance(0);
+
+    }
+
+    public static class ArticleFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.item_comment, container, false);
+        }
+        public static ArticleFragment newInstance(int someInt) {
+           ArticleFragment myFragment = new ArticleFragment();
+
+            Bundle args = new Bundle();
+            args.putInt("someInt", someInt);
+            myFragment.setArguments(args);
+
+            return myFragment;
+        }
+    }
+
+    public static class ArrayListFragment extends ListFragment {
+        int mNum;
+
+        /**
+         * Create a new instance of CountingFragment, providing "num"
+         * as an argument.
+         */
+        static ArrayListFragment newInstance(int num) {
+            ArrayListFragment f = new ArrayListFragment();
+
+            // Supply num input as an argument.
+            Bundle args = new Bundle();
+            args.putInt("num", num);
+            f.setArguments(args);
+
+            return f;
+        }
+
+        /**
+         * When creating, retrieve this instance's number from its arguments.
+         */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mNum = getArguments() != null ? getArguments().getInt("num") : 1;
+        }
+
+        /**
+         * The Fragment's UI is just a simple text view showing its
+         * instance number.
+         */
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.fragment_pager_list, container, false);
+            View tv = v.findViewById(R.id.text);
+            ((TextView)tv).setText("Monthly Income");
+            return v;
+        }
+
+
+        public String[] mApps = {
+                "Instagram",
+                "Pinterest",
+                "Pocket",
+                "Twitter"
+        };
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            setListAdapter(new ArrayAdapter<String>(getActivity(),
+                    android.R.layout.simple_list_item_1,mApps));
+    }
+
+        @Override
+        public void onListItemClick(ListView l, View v, int position, long id) {
+            Log.i("FragmentList", "Item clicked: " + id);
+        }
+    }
+}
